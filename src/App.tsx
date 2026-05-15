f3cec19ade02d8298bf6d8cd1923108c31be6e6d
The-Blight <hezer.fil@gmail.com>
2026-05-15 22:40:24 +0300

    Исправил ошибку автоформатирования

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <section id="center">
-        <div className="hero">
-        </div>
+        <div className="hero"></div>
         <div>
           <h1>Get started</h1>
           <p>
@@ -108,7 +107,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
